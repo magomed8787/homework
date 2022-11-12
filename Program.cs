abb8385a61f769426255a24126047d0f1586eb61
Magoved <python19871987@mail.ru>
2022-11-12 17:13:34 +0300

    finish prog1

--- Program.cs
+++ Program.cs
@@ -23,27 +23,3 @@ void PrintArray(string[] array)
 SecondArray(array1, array2);
 PrintArray(array2);
 
-// string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-// string[] array2 = new string[array1.Length];
-// void SecondArrayWithIF(string[] array1, string[] array2)
-// {
-//     int count = 0;
-//     for (int i = 0; i < array1.Length; i++)
-//     {
-//     if(array1[i].Length <= 3)
-//         {
-//         array2[count] = array1[i];
-//         count++;
-//         }
-//     }
-// }
-// void PrintArray(string[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
-// SecondArrayWithIF(array1, array2);
-// PrintArray(array2);
